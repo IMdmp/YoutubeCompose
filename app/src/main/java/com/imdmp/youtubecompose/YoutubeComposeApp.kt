2ee967134d8cce4cd0f9aa49395c7df6894eba77
Dominic Pagtalunan <pagtalunan.d@gmail.com>
2022-05-11 11:52:36 +0800

    closes #72

--- app/src/main/java/com/imdmp/youtubecompose/YoutubeComposeApp.kt
+++ app/src/main/java/com/imdmp/youtubecompose/YoutubeComposeApp.kt
@@ -2,11 +2,19 @@ package com.imdmp.youtubecompose
 
 import android.app.Application
 import android.content.Context
+import com.facebook.flipper.android.AndroidFlipperClient
+import com.facebook.flipper.android.utils.FlipperUtils
+import com.facebook.flipper.plugins.inspector.DescriptorMapping
+import com.facebook.flipper.plugins.inspector.InspectorFlipperPlugin
+import com.facebook.flipper.plugins.network.FlipperOkhttpInterceptor
+import com.facebook.flipper.plugins.network.NetworkFlipperPlugin
+import com.facebook.soloader.SoLoader
 import com.imdmp.youtubecompose.usecases.extractor.DownloaderImpl
 import com.imdmp.youtubecompose.usecases.extractor.DownloaderImpl.RECAPTCHA_COOKIES_KEY
 import dagger.hilt.android.HiltAndroidApp
 import kotlinx.coroutines.DEBUG_PROPERTY_NAME
 import kotlinx.coroutines.DEBUG_PROPERTY_VALUE_ON
+import okhttp3.OkHttpClient
 import org.schabi.newpipe.extractor.NewPipe
 import org.schabi.newpipe.extractor.downloader.Downloader
 import org.schabi.newpipe.extractor.localization.ContentCountry
@@ -14,17 +22,34 @@ import org.schabi.newpipe.extractor.localization.Localization
 import timber.log.Timber
 import java.util.*
 
+
 @HiltAndroidApp
 class YoutubeComposeApp : Application() {
     override fun onCreate() {
         super.onCreate()
+        SoLoader.init(this@YoutubeComposeApp, false)
+        val networkFlipperPlugin = NetworkFlipperPlugin()
+        val okhttpBuilder = OkHttpClient.Builder()
+        okhttpBuilder
+            .addNetworkInterceptor(FlipperOkhttpInterceptor(networkFlipperPlugin))
+
+        if (BuildConfig.DEBUG && FlipperUtils.shouldEnableFlipper(this@YoutubeComposeApp)) {
+            val client = AndroidFlipperClient.getInstance(this@YoutubeComposeApp)
+            client.addPlugin(
+                InspectorFlipperPlugin(
+                    this@YoutubeComposeApp,
+                    DescriptorMapping.withDefaults()
+                )
+            )
+            client.addPlugin(networkFlipperPlugin)
+            client.start()
+        }
+
         NewPipe.init(
-            getDownloader(),
+            getDownloader(okhttpBuilder),
             getPreferredLocalization(this),
             ContentCountry(Locale.getDefault().country)
         )
-
-        AppConfig.setup(this@YoutubeComposeApp)
 
         Timber.plant(Timber.DebugTree())
         System.setProperty(DEBUG_PROPERTY_NAME, DEBUG_PROPERTY_VALUE_ON)
@@ -39,8 +64,8 @@ class YoutubeComposeApp : Application() {
 
     }
 
-    protected fun getDownloader(): Downloader? {
-        val downloader = DownloaderImpl.init(null)
+    protected fun getDownloader(okhttpBuilder: OkHttpClient.Builder?): Downloader? {
+        val downloader = DownloaderImpl.init(okhttpBuilder)
         setCookiesToDownloader(downloader)
         return downloader
     }
