0a4351b3d5c757fac80e34b7b8a45ea25149a56e
Dominic Pagtalunan <pagtalunan.d@gmail.com>
2022-05-11 12:07:02 +0800

    updated app config #72

--- app/src/debug/java/com/imdmp/youtubecompose/AppConfig.kt
+++ app/src/debug/java/com/imdmp/youtubecompose/AppConfig.kt
@@ -3,17 +3,25 @@ package com.imdmp.youtubecompose
 import android.content.Context
 import com.facebook.flipper.android.AndroidFlipperClient
 import com.facebook.flipper.android.utils.FlipperUtils
-import com.facebook.flipper.core.FlipperClient
 import com.facebook.flipper.plugins.inspector.DescriptorMapping
 import com.facebook.flipper.plugins.inspector.InspectorFlipperPlugin
+import com.facebook.flipper.plugins.network.FlipperOkhttpInterceptor
+import com.facebook.flipper.plugins.network.NetworkFlipperPlugin
 import com.facebook.flipper.plugins.sharedpreferences.SharedPreferencesFlipperPlugin
 import com.facebook.soloader.SoLoader
+import okhttp3.OkHttpClient
+import timber.log.Timber
 
 object AppConfig {
-    fun setup(context: Context) {
+    fun setup(context: Context): OkHttpClient.Builder {
+        val okHttpBuilder = OkHttpClient.Builder()
+
         if (BuildConfig.DEBUG && FlipperUtils.shouldEnableFlipper(context)) {
+            val networkFlipperPlugin = NetworkFlipperPlugin()
             SoLoader.init(context, false)
 
+            okHttpBuilder
+                .addNetworkInterceptor(FlipperOkhttpInterceptor(networkFlipperPlugin))
             AndroidFlipperClient.getInstance(context).apply {
                 addPlugin(
                     InspectorFlipperPlugin(
@@ -24,9 +32,14 @@ object AppConfig {
                 addPlugin(
                     SharedPreferencesFlipperPlugin(context)
                 )
+                addPlugin(networkFlipperPlugin)
+
                 this.start()
 
             }
         }
+
+        Timber.plant(Timber.DebugTree())
+        return okHttpBuilder
     }
 }
--- app/src/main/java/com/imdmp/youtubecompose/YoutubeComposeApp.kt
+++ app/src/main/java/com/imdmp/youtubecompose/YoutubeComposeApp.kt
@@ -1,14 +1,6 @@
 package com.imdmp.youtubecompose
 
 import android.app.Application
-import android.content.Context
-import com.facebook.flipper.android.AndroidFlipperClient
-import com.facebook.flipper.android.utils.FlipperUtils
-import com.facebook.flipper.plugins.inspector.DescriptorMapping
-import com.facebook.flipper.plugins.inspector.InspectorFlipperPlugin
-import com.facebook.flipper.plugins.network.FlipperOkhttpInterceptor
-import com.facebook.flipper.plugins.network.NetworkFlipperPlugin
-import com.facebook.soloader.SoLoader
 import com.imdmp.youtubecompose.usecases.extractor.DownloaderImpl
 import com.imdmp.youtubecompose.usecases.extractor.DownloaderImpl.RECAPTCHA_COOKIES_KEY
 import dagger.hilt.android.HiltAndroidApp
@@ -19,7 +11,6 @@ import org.schabi.newpipe.extractor.NewPipe
 import org.schabi.newpipe.extractor.downloader.Downloader
 import org.schabi.newpipe.extractor.localization.ContentCountry
 import org.schabi.newpipe.extractor.localization.Localization
-import timber.log.Timber
 import java.util.*
 
 
@@ -27,50 +18,30 @@ import java.util.*
 class YoutubeComposeApp : Application() {
     override fun onCreate() {
         super.onCreate()
-        SoLoader.init(this@YoutubeComposeApp, false)
-        val networkFlipperPlugin = NetworkFlipperPlugin()
-        val okhttpBuilder = OkHttpClient.Builder()
-        okhttpBuilder
-            .addNetworkInterceptor(FlipperOkhttpInterceptor(networkFlipperPlugin))
-
-        if (BuildConfig.DEBUG && FlipperUtils.shouldEnableFlipper(this@YoutubeComposeApp)) {
-            val client = AndroidFlipperClient.getInstance(this@YoutubeComposeApp)
-            client.addPlugin(
-                InspectorFlipperPlugin(
-                    this@YoutubeComposeApp,
-                    DescriptorMapping.withDefaults()
-                )
-            )
-            client.addPlugin(networkFlipperPlugin)
-            client.start()
-        }
+        val okhttpBuilder = AppConfig.setup(this)
 
         NewPipe.init(
             getDownloader(okhttpBuilder),
-            getPreferredLocalization(this),
+            getPreferredLocalization(),
             ContentCountry(Locale.getDefault().country)
         )
-
-        Timber.plant(Timber.DebugTree())
         System.setProperty(DEBUG_PROPERTY_NAME, DEBUG_PROPERTY_VALUE_ON)
     }
 
 
-    fun getPreferredLocalization(
-        context: Context
-    ): Localization? {
+    private fun getPreferredLocalization(): Localization? {
         return Localization
             .fromLocale(Locale.getDefault())
 
     }
 
-    protected fun getDownloader(okhttpBuilder: OkHttpClient.Builder?): Downloader? {
+    private fun getDownloader(okhttpBuilder: OkHttpClient.Builder?): Downloader? {
         val downloader = DownloaderImpl.init(okhttpBuilder)
         setCookiesToDownloader(downloader)
         return downloader
     }
 
-    protected fun setCookiesToDownloader(downloader: DownloaderImpl) {
+    private fun setCookiesToDownloader(downloader: DownloaderImpl) {
         val prefs = androidx.preference.PreferenceManager.getDefaultSharedPreferences(
             applicationContext
         )
