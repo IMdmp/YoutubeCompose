d0f5c6e251931ad148d776d13919e5fd9c92dcd4
IMdmp <pagtalunan.d@gmail.com>
2022-05-22 21:28:44 +0800

    WIP motionlayout animation is working!!

--- youtubecompose-ui/src/main/java/com/imdmp/youtubecompose_ui/ui_player/VideoPlayerScreen.kt
+++ youtubecompose-ui/src/main/java/com/imdmp/youtubecompose_ui/ui_player/VideoPlayerScreen.kt
@@ -1,26 +1,31 @@
 package com.imdmp.youtubecompose_ui.ui_player
 
-import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.animation.core.tween
+import android.transition.TransitionSet
+import android.util.Log
+import androidx.compose.animation.core.*
+import androidx.compose.animation.splineBasedDecay
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.Orientation
-import androidx.compose.foundation.gestures.draggable
-import androidx.compose.foundation.gestures.rememberDraggableState
+import androidx.compose.foundation.gestures.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.input.pointer.positionChange
+import androidx.compose.ui.input.pointer.util.VelocityTracker
 import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.*
@@ -49,6 +54,10 @@ import compose.icons.fontawesomeicons.regular.ShareSquare
 import compose.icons.fontawesomeicons.regular.ThumbsDown
 import compose.icons.fontawesomeicons.regular.ThumbsUp
 import kotlinx.coroutines.NonDisposableHandle.parent
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+import kotlin.math.absoluteValue
+import kotlin.math.roundToInt
 
 
 const val VIDEO_PLAYER = "videoPlayer"
@@ -73,7 +82,7 @@ fun videoPlayerScreenConstraints(): ConstraintSet {
         val comments = createRefFor(COMMENTS)
         val surface = createRefFor(SURFACE)
 
-        constrain(surface){
+        constrain(surface) {
             height = Dimension.matchParent
             width = Dimension.matchParent
             top.linkTo(parent.top)
@@ -137,7 +146,7 @@ fun collapseVideoPlayerScreenConstraints(): ConstraintSet {
         val comments = createRefFor(COMMENTS)
         val surface = createRefFor(SURFACE)
 
-        constrain(surface){
+        constrain(surface) {
             start.linkTo(parent.start)
             end.linkTo(parent.end)
             bottom.linkTo(parent.bottom)
@@ -146,38 +155,38 @@ fun collapseVideoPlayerScreenConstraints(): ConstraintSet {
 
         }
 
-        constrain(videoPlayer){
+        constrain(videoPlayer) {
             start.linkTo(parent.start)
             bottom.linkTo(parent.bottom)
             height = Dimension.preferredValue(100.dp)
         }
 
-        constrain(titleBar){
-            start.linkTo(videoPlayer.end,16.dp)
-            top.linkTo(surface.top,16.dp)
+        constrain(titleBar) {
+            start.linkTo(videoPlayer.end, 16.dp)
+            top.linkTo(surface.top, 16.dp)
         }
 
-        constrain(progressIndicator){
+        constrain(progressIndicator) {
             visibility = Visibility.Gone
             bottom.linkTo(parent.bottom)
         }
-        constrain(iconActionBar){
+        constrain(iconActionBar) {
             top.linkTo(videoAuthorInfoBar.bottom)
             visibility = Visibility.Gone
         }
-        constrain(videoAuthorInfoBar){
+        constrain(videoAuthorInfoBar) {
             top.linkTo(titleBar.bottom)
             visibility = Visibility.Gone
         }
-        constrain(commentSeparatorLine){
+        constrain(commentSeparatorLine) {
             top.linkTo(iconActionBar.bottom, 8.dp)
             visibility = Visibility.Gone
         }
-        constrain(commentRow){
+        constrain(commentRow) {
             top.linkTo(commentSeparatorLine.bottom, 8.dp)
             visibility = Visibility.Gone
         }
-        constrain(comments){
+        constrain(comments) {
             top.linkTo(commentRow.bottom, 8.dp)
             visibility = Visibility.Gone
         }
@@ -224,55 +233,93 @@ fun VideoPlayerScreen(
     )
 
     var offsetY by remember { mutableStateOf(0f) }
+    val number = remember {
+        Animatable(0f)
+    }
+    val scope = rememberCoroutineScope()
+    val startNumber = remember{ mutableStateOf(0f) }
     val draggableState = rememberDraggableState {
         offsetY += it
+        scope.launch {
+            number.snapTo((offsetY / 1000).coerceAtLeast(0f).coerceAtMost(1f))
+
+        }
     }
-    val number = (offsetY / 1000).coerceAtLeast(0f).coerceAtMost(1f);
 
-        MotionLayout(
-            modifier = modifier.fillMaxSize(),
-            start = videoPlayerScreenConstraints(),
-            end = collapseVideoPlayerScreenConstraints(),
-            progress =number,
+    MotionLayout(
+        modifier = modifier.fillMaxSize(),
+        start = videoPlayerScreenConstraints(),
+        end = collapseVideoPlayerScreenConstraints(),
+        progress = number.value,
+    ) {
 
-            ) {
+        Box(
+            Modifier
+                .layoutId(SURFACE)
+                .background(Color.White)
+                .fillMaxSize()
+        ) {
 
-            Box(Modifier.layoutId(SURFACE).background(Color.White).fillMaxSize()) {
+        }
+        Playback(
+            Modifier
+                .aspectRatio(16 / 9f)
+                .layoutId(VIDEO_PLAYER)
+                .clickable {
+                    controlsVisible = !controlsVisible
+                }
+                .draggable(
+                    orientation = Orientation.Vertical,
+                    state = draggableState,
+                    onDragStopped = {
+                        if(startNumber.value==0f){
+                            if (number.value > 0.1f) {
+                                val res = number.animateTo(1f)
+                                startNumber.value = res.endState.value
+                                offsetY=1000f
+                            }else{
+                                val res = number.animateTo(0f)
+                                startNumber.value = res.endState.value
+                                offsetY = 0f
+                            }
+                        }else{
+                            if (number.value < 0.9f) {
+                                val res = number.animateTo(0f)
+                                startNumber.value = res.endState.value
+                                offsetY = 0f
+                            }else{
+                                val res = number.animateTo(1f)
+                                startNumber.value = res.endState.value
+                                offsetY=1000f
+                            }
+                        }
 
-            }
-            Playback(
-                Modifier
-                    .aspectRatio(16 / 9f)
-                    .layoutId(VIDEO_PLAYER)
-                    .clickable {
-                        controlsVisible = !controlsVisible
+                        Log.d("TAG","start number becomes: $startNumber")
+
                     }
-                    .draggable(
-                        orientation = Orientation.Vertical,
-                        state = draggableState
-                    )
-                    .testTag(Tags.TEST),
-                player = player,
-                playerScreenCallbacks = videoPlayerScreenCallbacks,
-                onUpdate = { playerView ->
-                    when (state.playerStatus) {
-                        PlayerStatus.PAUSED -> {
-                            playerView.player?.pause()
-                        }
-                        PlayerStatus.PLAYING -> {
-                            playerView.player?.play()
-                        }
+                )
+                .testTag(Tags.TEST),
+            player = player,
+            playerScreenCallbacks = videoPlayerScreenCallbacks,
+            onUpdate = { playerView ->
+                when (state.playerStatus) {
+                    PlayerStatus.PAUSED -> {
+                        playerView.player?.pause()
+                    }
+                    PlayerStatus.PLAYING -> {
+                        playerView.player?.play()
                     }
                 }
-            )
-
-            if (state.playerStatus == PlayerStatus.LOADING) {
-                CircularProgressIndicator(
-                    color = Color.Blue,
-                    modifier = Modifier
-                        .layoutId(PROGRESS_INDICATOR)
-                )
             }
+        )
+
+        if (state.playerStatus == PlayerStatus.LOADING) {
+            CircularProgressIndicator(
+                color = Color.Blue,
+                modifier = Modifier
+                    .layoutId(PROGRESS_INDICATOR)
+            )
+        }
 //
 //        Controls(
 //            controlsCallback = videoPlayerScreenCallbacks,
@@ -294,43 +341,43 @@ fun VideoPlayerScreen(
 //        )
 
 
-            TitleBar(
-                modifier = Modifier
-                    .padding(top = 8.dp, bottom = 8.dp)
-                    .layoutId(TITLE_BAR), state = state
-            )
-            VideoAuthorInfoBar(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(top = 4.dp, bottom = 4.dp)
-                    .layoutId(VIDEO_AUTHOR_INFO_BAR), state = state
-            )
-            IconActionsBar(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .layoutId(ICON_ACTION_BAR), state = state
-            )
+        TitleBar(
+            modifier = Modifier
+                .padding(top = 8.dp, bottom = 8.dp)
+                .layoutId(TITLE_BAR), state = state
+        )
+        VideoAuthorInfoBar(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 4.dp, bottom = 4.dp)
+                .layoutId(VIDEO_AUTHOR_INFO_BAR), state = state
+        )
+        IconActionsBar(
+            modifier = Modifier
+                .fillMaxWidth()
+                .layoutId(ICON_ACTION_BAR), state = state
+        )
 
-            Box(
-                modifier = Modifier
-                    .height(1.dp)
-                    .fillMaxWidth()
-                    .background(Color.LightGray)
-                    .layoutId(COMMENT_SEPARATOR_LINE)
-            )
+        Box(
+            modifier = Modifier
+                .height(1.dp)
+                .fillMaxWidth()
+                .background(Color.LightGray)
+                .layoutId(COMMENT_SEPARATOR_LINE)
+        )
 
-            CommentHeaderRow(modifier = Modifier.layoutId(COMMENT_ROW))
+        CommentHeaderRow(modifier = Modifier.layoutId(COMMENT_ROW))
 
-            Comments(
-                listState = listState,
-                modifier = Modifier
-                    .layoutId(COMMENTS),
-                commentState = CommentState(
-                    commentModelList = state.commentList,
-                    isLoading = false,
-                    error = null
-                ),
-            )
+        Comments(
+            listState = listState,
+            modifier = Modifier
+                .layoutId(COMMENTS),
+            commentState = CommentState(
+                commentModelList = state.commentList,
+                isLoading = false,
+                error = null
+            ),
+        )
 
 
 //        HorizontalPager(
@@ -393,8 +440,67 @@ fun VideoPlayerScreen(
 //                )
 //            }
 //        }
-        }
+    }
 
+}
+
+fun Modifier.swipeToDismiss(
+    onDismissed: () -> Unit
+): Modifier = composed {
+    val offsetX = remember { Animatable(0f) }
+    pointerInput(Unit) {
+        // Used to calculate fling decay.
+        val decay = splineBasedDecay<Float>(this)
+        // Use suspend functions for touch events and the Animatable.
+        coroutineScope {
+            while (true) {
+                // Detect a touch down event.
+                val pointerId = awaitPointerEventScope { awaitFirstDown().id }
+                val velocityTracker = VelocityTracker()
+                // Stop any ongoing animation.
+                offsetX.stop()
+                awaitPointerEventScope {
+                    horizontalDrag(pointerId) { change ->
+                        // Update the animation value with touch events.
+                        launch {
+                            offsetX.snapTo(
+                                offsetX.value + change.positionChange().x
+                            )
+                        }
+                        velocityTracker.addPosition(
+                            change.uptimeMillis,
+                            change.position
+                        )
+                    }
+                }
+                // No longer receiving touch events. Prepare the animation.
+                val velocity = velocityTracker.calculateVelocity().x
+                val targetOffsetX = decay.calculateTargetValue(
+                    offsetX.value,
+                    velocity
+                )
+                // The animation stops when it reaches the bounds.
+                offsetX.updateBounds(
+                    lowerBound = -size.width.toFloat(),
+                    upperBound = size.width.toFloat()
+                )
+                launch {
+                    if (targetOffsetX.absoluteValue <= size.width) {
+                        // Not enough velocity; Slide back.
+                        offsetX.animateTo(
+                            targetValue = 0f,
+                            initialVelocity = velocity
+                        )
+                    } else {
+                        // The element was swiped away.
+                        offsetX.animateDecay(velocity, decay)
+                        onDismissed()
+                    }
+                }
+            }
+        }
+    }
+        .offset { IntOffset(offsetX.value.roundToInt(), 0) }
 }
 
 @Composable
