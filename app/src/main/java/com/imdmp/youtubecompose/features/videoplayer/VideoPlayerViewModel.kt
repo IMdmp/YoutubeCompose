db2f33da97e6752c4270df1a622630ed1e89d8d4
Dominic Pagtalunan <pagtalunan.d@gmail.com>
2022-08-05 16:37:59 +0800

    playable vids

--- app/src/main/java/com/imdmp/youtubecompose/features/videoplayer/VideoPlayerViewModel.kt
+++ app/src/main/java/com/imdmp/youtubecompose/features/videoplayer/VideoPlayerViewModel.kt
@@ -2,26 +2,43 @@ package com.imdmp.youtubecompose.features.videoplayer
 
 import android.net.Uri
 import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.viewModelScope
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.MediaItem
+import com.imdmp.datarepository.YoutubeRepository
 import com.imdmp.youtubecompose.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @HiltViewModel
-class VideoPlayerViewModel @Inject constructor(var videoPlayer: ExoPlayer) : BaseViewModel(),
+class VideoPlayerViewModel @Inject constructor(
+    var videoPlayer: ExoPlayer,
+    val youtubeRepository: YoutubeRepository
+) : BaseViewModel(),
     VideoPlayerViewCallbacks {
     val videoMode = mutableStateOf(VideoMode.NORMAL)
     val url = mutableStateOf("")
 
     fun initVideoPlayer(url: String) {
-        val uri =
-            Uri.parse(url)
+        viewModelScope.launch(Dispatchers.IO) {
+            val videoData = youtubeRepository.getVideoDataInfo(url)
+            val resUrl = videoData.streamList.last()
+            val uri =
+                Uri.parse(resUrl.url)
+            val mediaItem: MediaItem = MediaItem.fromUri(uri)
 
-        val mediaItem: MediaItem = MediaItem.fromUri(uri)
-        videoPlayer.setMediaItem(mediaItem)
-        videoPlayer.prepare()
-        videoPlayer.play()
+            withContext(Dispatchers.Main) {
+                videoPlayer.setMediaItem(mediaItem)
+                videoPlayer.prepare()
+                videoPlayer.play()
+            }
+
+        }
+
+
     }
 
     override fun goFullScreen() {
