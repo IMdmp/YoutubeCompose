180e8982881fb133192dab0244db9927a8a4cfd5
Dominic Pagtalunan <pagtalunan.d@gmail.com>
2022-08-10 00:02:47 +0800

    Remove old code (#87)

--- videoplayer/src/main/java/com/imdmp/videoplayer/usecase/PrepareAndPlayerVideoUseCase.kt
+++
@@ -1,9 +0,0 @@
-package com.imdmp.videoplayer.usecase
-
-import com.google.android.exoplayer2.ExoPlayer
-
-interface PrepareAndPlayerVideoUseCase {
-    suspend operator fun invoke(
-        streamUrl: String, player: ExoPlayer
-    )
-}
--- youtubecompose-ui/src/main/java/com/imdmp/youtubecompose_ui/search/SearchState.kt
+++
@@ -1,7 +0,0 @@
-package com.imdmp.youtubecompose_ui.search
-
-data class SearchState(
-    val suggestionList: MutableList<String> = mutableListOf(),
-    val searchText: String = ""
-) {
-}
