4d3db44a5619e416ad91e781f00fd6678b24da51
Dominic Pagtalunan <pagtalunan.d@gmail.com>
2022-08-12 23:25:19 +0800

    experiment test1

--- app/src/main/java/com/imdmp/youtubecompose/features/videoplayer/DraggableVideoPlayer.kt
+++ app/src/main/java/com/imdmp/youtubecompose/features/videoplayer/DraggableVideoPlayer.kt
@@ -5,12 +5,15 @@ import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.gestures.draggable
 import androidx.compose.foundation.gestures.rememberDraggableState
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Surface
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalLifecycleOwner
-import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidViewBinding
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
 import androidx.lifecycle.LifecycleOwner
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.ui.StyledPlayerView
@@ -53,7 +56,7 @@ fun DraggableVideoPlayer(
         exoPlayer = videoPlayerViewModel.videoPlayer,
         videoPlayerViewCallbacks = videoPlayerViewModel,
         content = {
-            Surface(modifier = Modifier.fillMaxSize()) {
+            Surface(modifier = it.fillMaxSize()) {
                 VideoPlayerDesc(videoPlayerViewModel.getState())
             }
         }
@@ -66,7 +69,7 @@ fun DraggableVidPlayer(
     exoPlayer: ExoPlayer,
     videoPlayerViewCallbacks: VideoPlayerViewCallbacks? = null,
     callback: (progress: Float) -> Unit = {},
-    content: @Composable () -> Unit = { Surface {} }
+    content: @Composable (modifier: Modifier) -> Unit = { Surface {} }
 ) {
     var playerView1: StyledPlayerView? = null
     val screenMotionProgress = remember {
@@ -95,51 +98,59 @@ fun DraggableVidPlayer(
     LaunchedEffect(key1 = windowState.value) {
         playerView1?.useController = windowState.value != PlayerWindowState.COLLAPSED
     }
-
-    AndroidViewBinding(
-        modifier = modifier
-            .fillMaxSize()
-            .draggable(
-                orientation = Orientation.Vertical,
-                state = draggableState,
-                onDragStopped = {
-                    if (windowState.value == PlayerWindowState.NORMAL) {
-                        offsetY = if (screenMotionProgress.value > 0.1f) {
-                            screenMotionProgress.animateTo(1f).endState.value
-                            windowState.value = PlayerWindowState.COLLAPSED
-                            1000f
+    ConstraintLayout {
+        val (player, additionalContent) = createRefs()
+        AndroidViewBinding(
+            modifier = modifier
+                .fillMaxWidth()
+                .draggable(
+                    orientation = Orientation.Vertical,
+                    state = draggableState,
+                    onDragStopped = {
+                        if (windowState.value == PlayerWindowState.NORMAL) {
+                            offsetY = if (screenMotionProgress.value > 0.1f) {
+                                screenMotionProgress.animateTo(1f).endState.value
+                                windowState.value = PlayerWindowState.COLLAPSED
+                                1000f
+                            } else {
+                                screenMotionProgress.animateTo(0f)
+                                0f
+                            }
                         } else {
-                            screenMotionProgress.animateTo(0f)
-                            0f
-                        }
-                    } else {
-                        offsetY = if (screenMotionProgress.value < 0.9f) {
-                            screenMotionProgress.animateTo(0f)
-                            windowState.value = PlayerWindowState.NORMAL
-                            0f
-                        } else {
-                            screenMotionProgress.animateTo(1f)
-                            1000f
+                            offsetY = if (screenMotionProgress.value < 0.9f) {
+                                screenMotionProgress.animateTo(0f)
+                                windowState.value = PlayerWindowState.NORMAL
+                                0f
+                            } else {
+                                screenMotionProgress.animateTo(1f)
+                                1000f
+                            }
                         }
                     }
-                }
-            ),
-        factory = VideoPlayerBinding::inflate) {
-        playerView1 = this.playerView
-        this.playerView.player = exoPlayer
-        this.root.progress = screenMotionProgress.value
-        this.playerView.findViewById<IconicsImageView>(R.id.fullscreen).setOnClickListener {
-            videoPlayerViewCallbacks?.goFullScreen()
-        }
+                )
+                .constrainAs(player) {
+                    top.linkTo(parent.top)
+                },
+            factory = VideoPlayerBinding::inflate
+        ) {
+            playerView1 = this.playerView
+            this.playerView.player = exoPlayer
+            this.root.progress = screenMotionProgress.value
+            this.playerView.findViewById<IconicsImageView>(R.id.fullscreen).setOnClickListener {
+                videoPlayerViewCallbacks?.goFullScreen()
+            }
 
-        this.closeButton.setOnClickListener {
-            videoPlayerViewCallbacks?.closeButtonClicked()
-        }
-        this.composeView.apply {
-            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
-            setContent {
-                content()
+            this.closeButton.setOnClickListener {
+                videoPlayerViewCallbacks?.closeButtonClicked()
             }
         }
+        content(Modifier
+            .constrainAs(additionalContent) {
+                top.linkTo(parent.top, margin = 300.dp)
+                start.linkTo(parent.start)
+                end.linkTo(parent.end)
+                Dimension.Companion.fillToConstraints
+            })
+
     }
 }
